b5a953c901f03992aa9c9b0bb7fa3a47f6cf32cf
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-04-23 00:15:46 +0300

    sum2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
-export const App = () => {
+import PropTypes from 'prop-types';
+import { Profile } from 'components/Profile/Profile';
+
+export const App = ({ username, tag, location, avatar, followers, views, likes }) => {
   return (
     <div
       style={{
@@ -10,7 +13,30 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        key={username}
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        followers={stats.followers}
+        views={stats.views}
+        likes={stats.likes}
+      />
+      {/* React homework template */}
     </div>
   );
 };
+
+App.PropTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.exact ({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    })
+}
+    
