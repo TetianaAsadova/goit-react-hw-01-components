527f1a13807bf53ac47ce0f5f511dde7dc9f1456
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-04-24 00:16:29 +0300

    sum3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
-import { Profile } from 'components/Profile/Profile';
+import user from '../user.json';
+import { Profile } from './Profile/Profile';
 
-export const App = ({ username, tag, location, avatar, followers, views, likes }) => {
+export const App = () => {
   return (
     <div
       style={{
@@ -13,30 +14,15 @@ export const App = ({ username, tag, location, avatar, followers, views, likes }
         color: '#010101'
       }}
     >
-      <Profile
-        key={username}
-        username={username}
-        tag={tag}
-        location={location}
-        avatar={avatar}
-        followers={stats.followers}
-        views={stats.views}
-        likes={stats.likes}
-      />
-      {/* React homework template */}
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />   
     </div>
   );
 };
 
-App.PropTypes = {
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    stats: PropTypes.exact ({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-    })
-}
     
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
-export const Profile = ({ username, tag, location, avatar, followers, views, likes }) => {
+export const Profile = ({ username, tag, location, avatar, stats }) => {
     return (
     <div className={css.profile}>
             <div className={css.description}>
@@ -19,15 +19,15 @@ export const Profile = ({ username, tag, location, avatar, followers, views, lik
             <ul className={css.stats}>
                 <li>
                     <span className={css.label}>Followers</span>
-                    <span className={css.quantity}>{followers}</span>
+                    <span className={css.quantity}>{stats.followers}</span>
                 </li>
                 <li>
                     <span className={css.label}>Views</span>
-                    <span className={css.quantity}>{views}</span>
+                    <span className={css.quantity}>{stats.views}</span>
                 </li>
                 <li>
                     <span className={css.label}>Likes</span>
-                    <span className={css.quantity}>{likes}</span>
+                    <span className={css.quantity}>{stats.likes}</span>
                 </li>
             </ul>
         </div>
