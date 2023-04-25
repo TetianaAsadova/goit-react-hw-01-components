c441f0dfce44715f23470087d29084bf3be67e48
TetianaAsadova <114417194+TetianaAsadova@users.noreply.github.com>
2023-04-26 00:51:45 +0300

    sum5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import PropTypes from 'prop-types';
 import user from '../user.json';
 import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
-
+import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
-  
-  // console.log(`user`, user);
   return (
     <div
       style={{
@@ -25,17 +25,13 @@ export const App = () => {
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}  
-        // followers={user.stats.followers}
-        // views={user.stats.views}
-        // likes={user.stats.likes}
       />
       
-      {/* <Statistics title="Upload stats" stats={data} /> */}
-  
-      <Statistics stats={data} />
-  
+      <Statistics title="Upload stats" stats={data} />
 
+      <FriendList friends={friends} />
       
+      <TransactionHistory items={transactions} />
       
     </div>
   );
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,29 +2,16 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
 export const Statistics = ({ title, stats }) => {
-    console.log(`stats`, stats);
     return (
         <section className={css.statistics}>
-            
-                <h2 className={css.title}>{title}</h2>
-            
+            {title !== '' && (<h2 className={css.title}>{title}</h2>)} 
             <ul className={css.statList}>
-                <li className={css.item}>
-                    <span className={css.label}>{stats.label}</span>
-                    <span className={css.percentage}>{stats.percentage}%</span>
-                </li>
-                <li className={css.item}>
-                    <span className={css.label}>{stats.label}</span>
-                    <span className={css.percentage}>{stats.percentage}%</span>
-                </li>
-                <li className={css.item}>
-                    <span className={css.label}>{stats.label}</span>
-                    <span className={css.percentage}>{stats.percentage}%</span>
-                </li>
-                <li className={css.item}>
-                    <span className={css.label}>{stats.label}</span>
-                    <span className={css.percentage}>{stats.percentage}%</span>
-                </li>
+                {stats.map(stat => 
+                    <li key={stat.id} className={css.item}>
+                        <span className={css.label}>{stat.label}</span>
+                        <span className={css.percentage}>{stat.percentage}%</span>
+                    </li>
+                )}
             </ul>
         </section>
     );
